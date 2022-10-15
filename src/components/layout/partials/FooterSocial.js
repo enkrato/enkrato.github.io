87a611113730f643009776b5362833d8861663e4
Albert Boateng <albertboateng@Alberts-MacBook-Pro.local>
2022-10-15 17:10:23 -0400

    testing_enkrato_app

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -18,7 +18,7 @@ const FooterSocial = ({
     >
       <ul className="list-reset">
         <li>
-          <a href="https://facebook.com/">
+          <a href="https://enkrato.page.link/verifyshipper">
             <svg
               width="16"
               height="16"
