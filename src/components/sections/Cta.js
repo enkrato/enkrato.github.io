c7974c9f7534738fef2b65ebd0c50383e09db056
enkrato <enkrato@gmail.com>
2023-11-01 18:37:14 -0400

    make_changes_to_master

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -52,7 +52,7 @@ const Cta = ({
           className={innerClasses}
         >
           <div className="cta-slogan">
-            <p style="font-size: 14px; line-height: 140%;"><strong>Questions? </strong>Contact us anytime at <strong>enkrato@gmail.com</strong></p>
+          <p style="font-size: 14px; line-height: 140%;"><strong>Questions? </strong>Contact us anytime at <strong>enkrato@gmail.com</strong></p>
           </div>
           <div className="cta-action">
             <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="right" placeholder="Your best email">
